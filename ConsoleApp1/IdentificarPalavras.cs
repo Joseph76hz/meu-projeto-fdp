ddc20bab10c78845e3968891295f089a1df58e04
Joseph76hz <josecarlos13br@gmail.com>
2025-11-28 16:28:08 -0300

    PalavraReversaTeste

--- ConsoleApp1/IdentificarPalavras.cs
+++ ConsoleApp1/IdentificarPalavras.cs
@@ -8,7 +8,7 @@ namespace Texto
         public static void Palavra()
         {
            string text  = "Buracos negros são regiões do espaço-tempo com gravidade tão intensa que nem a luz pode escapar, formados pelo colapso de estrelas massivas ou acúmulo de matéria no centro das galáxias. Eles são invisíveis, mas podem ser detectados pelos efeitos que causam em sua vizinhança, como o movimento de estrelas ao redor. A primeira imagem de um buraco negro foi capturada em 2019 pelo projeto Event Horizon Telescope. ";
-           string procurar = "mas";
+           string procurar = "luz";
            MatchCollection matches = Regex.Matches(text, procurar);
             foreach (Match n in matches)
             {
